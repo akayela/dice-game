714b9e1f5fb52ee685f3b4f3887fd4ab6fbf226f
Audience Kayela <akayela22@gmail.com>
2024-06-20 13:13:49 +0200

    Dice roll functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//Selecting elements
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
