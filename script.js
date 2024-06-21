1580c1c49fb764eb76037bedd0cb2462f2bbec6c
Audience Kayela <akayela22@gmail.com>
2024-06-21 05:22:19 +0200

    Switching between players

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 'use strict';
 
 //Selecting elements
+const palyer0El = document.querySelector('.player--0');
+const palyer1El = document.querySelector('.player--1');
 
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+//Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling the dice
 btnRoll.addEventListener('click', function () {
+  // Generating random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  //Check for rolled 1:
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // display the score
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    palyer0El.classList.toggle('player--active');
+    palyer1El.classList.toggle('player--active');
+  }
 });
